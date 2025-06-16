e718d96ee9e4b6343f518c45abd0a8765508e784
larry <lijiang64@huawei.com>
2025-06-16 07:25:36 +0000

    chore: Copyright注释头使用统一lua注释风格

--- test/integration/test_account.lua
+++ test/integration/test_account.lua
@@ -1,8 +1,7 @@
 -- Copyright (c) 2024 Huawei Technologies Co., Ltd.
 -- openUBMC is licensed under Mulan PSL v2.
 -- You can use this software according to the terms and conditions of the Mulan PSL v2.
--- You may obtain a copy of Mulan PSL v2 at:
---         http://license.coscl.org.cn/MulanPSL2
+-- You may obtain a copy of Mulan PSL v2 at: http://license.coscl.org.cn/MulanPSL2
 -- THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
 -- EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 -- MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
@@ -30,6 +29,8 @@ require 'account.json_types.Rule'
 require 'account.json_types.LocalAccountAuthN'
 require 'account.json_types.SnmpUser'
 require 'account.json_types.AccountPolicy'
+require 'lualib.infrastructure.file_proxy'
+require 'lualib.interface.dump'
 
 log:set_log_module_name('account_IT')
 log:set_debug_log_type(log.OUT_TYPE_LOCAL)
