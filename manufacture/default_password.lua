50a8ba2d7a6892e88b77841c171f040a47825fb9
xcy77 <1509471562@qq.com>
2026-03-24 17:27:57 +0800

    Remove static manufacturing default credentials

--- manufacture/default_password.lua
+++ manufacture/default_password.lua
@@ -8,24 +8,19 @@
 -- See the Mulan PSL v2 for more details.
 local DEFAULT_PWD = {
     Admin = {
-        Password = [=[$6$AhtdE42u9JhRdPw1$7e3wQX6sfjwTEJr8UZjmAM3EDJMcM0AuSrK2aN7U]=] ..
-            [=[km0II0Mjm2wR8EAenfBv3SaJ/y4wCu5fqSYCqb4BJfrRd0]=],
-        IpmiPassword = [=[0000000200000000000000060000000500000007c3c90f687c9010341e1ab8ff309cfaad388113c]=] ..
-            [=[66f28f8f6ff48e203bfa7c31f010000010000000000000010126b5c4bb597331e5100ae9da8814a84]=],
+        Password = [=[]=],
+        IpmiPassword = [=[]=],
     },
     RoCommunity = {
-        IpmiPassword = [=[0000000200000000000000060000000500000007c3c90f687c901034dae1df20fc26413d5b0a09]=] ..
-            [=[53c98d8f63c320bb9a9655eee7010000010000000000000010d2cd60c6dc0e254c2ffd6c138f6e5d19]=],
+        IpmiPassword = [=[]=],
     },
     RwCommunity = {
-        IpmiPassword = [=[0000000200000000000000060000000500000007c3c90f687c90103440282a7633d3fcdc11bc54cb5c3f]=] ..
-            [=[3e33431e02241ff6f52601000001000000000000001094f0e7e79b93e98993b578df4482ed0e]=],
+        IpmiPassword = [=[]=],
     },
     SnmpUserInfo = {
-        AuthenticationKey = [=[7a2f5b86f20233ec7b7bef49643fbf34d86d0cbf6f1f8f5c52d4f30d4536938b]=],
-        EncryptionKey = [=[7a2f5b86f20233ec7b7bef49643fbf34d86d0cbf6f1f8f5c52d4f30d4536938b]=],
-        SNMPPassword = [=[$6$hqlSg3LmkBvBNpNW$eC4HKBS.8KzMPxFx/gdxhOnnrRUN8n7DDrEX92]=] ..
-            [=[GsMOWfuDCJNuxzVIjkdWwEidlqPhE.dMUmD.wcgSayjC8GF/]=]
+        AuthenticationKey = [=[]=],
+        EncryptionKey = [=[]=],
+        SNMPPassword = [=[]=]
     }
 }
 
